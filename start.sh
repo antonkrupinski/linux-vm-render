0305eeb33257bc6ef9d7cf0e017911f65b6c0018
antonkrupinski <antonkrupinski@icloud.com>
2026-02-10 20:24:23 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-# Start virtual framebuffer (headless X server)
+# Start virtual framebuffer
+Xvfb :1 -screen 0 1280x720x24 &
 export DISPLAY=:1
-Xvfb $DISPLAY -screen 0 1280x720x16 &
 
 # Start Fluxbox window manager
 fluxbox &
 
-# Start Chromium browser (optional: open Google on startup)
-chromium-browser --no-sandbox --disable-gpu &
+# Start x11vnc server
+x11vnc -display :1 -nopw -listen 0.0.0.0 -xkb -forever &
 
-# Start x11vnc to allow VNC connections
-x11vnc -display $DISPLAY -forever -nopw -listen 0.0.0.0 -shared &
+# Start noVNC web client (fixed path)
+/opt/novnc/launch.sh --vnc localhost:5900 --listen 8080 &
 
-# Start noVNC web client on port 8080
-/opt/novnc/utils/launch.sh --vnc 127.0.0.1:5900 --listen 8080
+# Start Chromium browser in virtual display
+chromium --no-sandbox --disable-gpu --start-maximized &
 
+# Keep container running
+tail -f /dev/null
